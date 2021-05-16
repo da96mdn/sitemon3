467d2ce2040e256da8536fad726c16601567e468
Mathias Dicklén <mathias.dicklen@gmail.com>
2021-05-16 13:31:27 +0200

    Test

--- src/main/java/bex/aq/sitemon/jaas/JaasAuthenticationTest.java
+++ src/main/java/bex/aq/sitemon/jaas/JaasAuthenticationTest.java
@@ -4,7 +4,7 @@ import javax.security.auth.login.LoginException;
 
 /**
  * Klass för att testa JAAS auktorisering
- * @author Mathias Dicklén
+ * @author Mathias Dicklén           test
  * @version 1.0
  */
 public class JaasAuthenticationTest
